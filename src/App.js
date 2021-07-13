8f17e90f4549fb244c273e01fe1b6cb34522cf58
Chris <cbenjamin2009@gmail.com>
2021-07-13 15:36:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  const element = <h1>Hello World, Lets get Tacos</h1>
+  return (
+    <div className="App">
+      {element}
+    </div>
+  );
+}
+
+export default App;
